82beb1f8bf475365a3f7842170a0eda69a639621
Gabriel de Carvalho <gabriel.de.carvalho_@hotmail.com>
2021-10-10 10:43:47 -0300

    refactor: Add new favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,24 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+            type="image/png"
+          />
+          <link
+            rel="shortcut icon"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+            type="image/png"
+          />
+          <link
+            rel="shortcut icon"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+            type="image/png"
+          />
           <meta name="application-name" content="my-portfolio" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
